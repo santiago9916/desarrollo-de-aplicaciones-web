464dd9570f9d9071212938ed6e52fb6852aa6c25
Milo <mimiiilan869@gmail.com>
2026-08-19 12:07:59 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import Micomponente from "./Micomponente";
-import { StrictMode } from "react";
-import {createRoot} from "react-dom/client";
- createRoot(document.getElementById("root")).render(
-    <StrictMode>
-    <Micomponente/>
-    </StrictMode>
- )
